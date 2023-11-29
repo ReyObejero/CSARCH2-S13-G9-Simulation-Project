82772f73c74aa3b5b057119a5bb96ec2451787a6
ReyObejero <rey_obejero@dlsu.edu.ph>
2023-11-29 19:51:54 +0800

    Built an executable jar file for the cache simulation system

---
+++ src/cache/Main_.java
@@ -0,0 +1,209 @@
+package cache;
+
+import cache.cacheparts.CacheEngine;
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.text.Font;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Main_ extends Application {
+
+    public static void main(String args[]){
+        launch(args);
+    }
+
+
+    @Override
+    public void start(Stage stage) {
+        stage.setTitle("Cache Simulator");
+        stage.setResizable(false);
+
+        GridPane gridPaneFirst = createCacheGrid();
+        GridPane gridPaneSecond = createCacheGrid();
+        GridPane gridPaneThird = createCacheGrid();
+
+        TextField textField = new TextField();
+        textField.setMinWidth(630); // Set preferred width to 300 pixels
+        textField.setMaxHeight(50); // Set preferred height to 50 pixels
+        textField.setFont(new Font(25));
+        textField.setPromptText("E.g 8");
+
+        Label label = new Label("Enter Memory Blocks Here (N):");
+        label.setFont(new Font(20));
+
+        Button forwardButton = new Button("Step Forward");
+        Button finalMemorySnapshotButton = new Button("Final Memory Snapshot");
+        Button endButton = new Button("End Current Execution");
+        Button startButton = new Button("Start Simulation");
+
+        forwardButton.setDisable(true);
+        finalMemorySnapshotButton.setDisable(true);
+
+        endButton.setDisable(true);
+
+        TextArea stepsArea = new TextArea();
+        stepsArea.setFont(new Font(16));
+        stepsArea.setEditable(false);
+
+        CacheEngine[] simulator = new CacheEngine[1];
+
+        textField.setOnAction(event -> {
+            startSimulation(simulator, textField, stepsArea, endButton, startButton);
+            forwardButton.setDisable(false);
+            finalMemorySnapshotButton.setDisable(false);
+        });
+
+        startButton.setOnAction(event -> {
+            startSimulation(simulator, textField, stepsArea, endButton, startButton);
+            forwardButton.setDisable(false);
+            finalMemorySnapshotButton.setDisable(false);
+        });
+
+
+        forwardButton.setOnAction(event -> {
+            boolean flag = simulator[0].performStep(CACHE_LINES);
+            if(flag){
+                stepsArea.clear();
+                stepsArea.appendText(simulator[0].getCurrentStepInformation());
+            }
+            else {
+                forwardButton.setDisable(true);
+                finalMemorySnapshotButton.setDisable(true);
+                showPopup("Simulation Complete.");
+            }
+        });
+
+        finalMemorySnapshotButton.setOnAction(event -> {
+            simulator[0].performAllStep(stepsArea, CACHE_LINES);
+            forwardButton.setDisable(true);
+            finalMemorySnapshotButton.setDisable(true);
+            showPopup("Simulation Complete.");
+        });
+
+        endButton.setOnAction(event -> {
+            if (simulator[0] != null) {
+                simulator[0] = null;
+                stepsArea.clear();
+                textField.setEditable(true);
+                endButton.setDisable(true);
+                startButton.setDisable(false);
+                textField.setDisable(false);
+                forwardButton.setDisable(true);
+                finalMemorySnapshotButton.setDisable(true);
+                textField.clear();
+                CACHE_LINES.forEach(i -> {
+                    i.forEach(h-> {
+                        h.forEach(j -> {
+                            j.setText("N/A");
+                        });
+                    });
+                });
+            }
+        });
+
+        HBox buttonsPane = new HBox(10, finalMemorySnapshotButton, forwardButton, endButton, startButton);
+        buttonsPane.setAlignment(Pos.TOP_CENTER);
+
+        HBox textPane = new HBox(textField);
+        textPane.setAlignment(Pos.TOP_CENTER);
+
+
+        VBox box = new VBox(20, label, textPane, buttonsPane, gridPaneFirst,gridPaneSecond,gridPaneThird, stepsArea);
+        box.setAlignment(Pos.TOP_CENTER);
+        VBox.setVgrow(stepsArea, Priority.ALWAYS);
+
+
+        Scene scene = new Scene(box, 900, 900);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    private static final int WINDOW_WIDTH = 850;
+    private static final int N_BLOCKS = 8;
+    private static final int COLUMN_WIDTH = (WINDOW_WIDTH - (N_BLOCKS + 1) * 10) / N_BLOCKS;
+    private static final int ROW_HEIGHT = 50;
+
+    public List<List<List<Label>>> CACHE_LINES = new ArrayList<>();
+    private GridPane createCacheGrid() {
+        CACHE_LINES.add(new ArrayList<>());
+        CACHE_LINES.get(CACHE_LINES.size()-1).add(new ArrayList<>());
+        CACHE_LINES.get(CACHE_LINES.size()-1).add(new ArrayList<>());
+
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(1); // Spacing between columns
+        grid.setVgap(1); // Spacing between rows
+        grid.setPadding(new Insets(10, 10, 10, 10)); // Padding around the grid
+
+        // Create header for Blocks
+        for (int block = 1; block <= N_BLOCKS; block++) {
+            Label blockLabel = createStyledLabel("Block " + block, COLUMN_WIDTH, ROW_HEIGHT, "8", true);
+            grid.add(blockLabel, block, 0);
+        }
+
+        // Create rows for Sets and MRU Status
+        for (int set = 1; set <= 2; set++) {
+            Label setLabel = createStyledLabel("Set " + set, COLUMN_WIDTH, ROW_HEIGHT, "6", true);
+
+            grid.add(setLabel, 0, set);
+
+            for (int block = 1; block <= N_BLOCKS; block++) {
+                Label cacheValue = createStyledLabel("N/A", COLUMN_WIDTH, ROW_HEIGHT, "8", false);
+                CACHE_LINES.get(CACHE_LINES.size()-1).get(set-1).add(cacheValue);
+                grid.add(cacheValue, block, set);
+            }
+        }
+
+        return grid;
+    }
+
+    private Label createStyledLabel(String text, int width, int height, String size, boolean bold) {
+        Label label = new Label(text);
+        label.setMinSize(width, height);
+        label.setMaxSize(width, height);
+        label.setPrefSize(width, height);
+        if(!bold){
+            label.setStyle("-fx-border-color: black; -fx-padding: " + size + "; -fx-alignment: center;");
+        }
+        else {
+            label.setStyle("-fx-font-weight: bold; -fx-border-color: black; -fx-padding: " + size + "; -fx-alignment: center;");
+        }
+        label.setWrapText(true);
+        return label;
+    }
+
+    private void startSimulation(CacheEngine[] engine, TextField textField, TextArea stepsArea, Button endButton, Button startButton) {
+        String text = textField.getText(); // Get input text
+
+        textField.setEditable(false);
+        textField.setDisable(true);
+        //MachinePartsInitializer initializer = new MachinePartsInitializer("toread.txt");
+        //MachineParts parts = initializer.initialize();
+
+        engine[0] = new CacheEngine(Integer.parseInt(text));
+        //stepsArea.appendText();
+        endButton.setDisable(false);
+        startButton.setDisable(true);
+    }
+
+    private void showPopup(String str) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Results");
+        alert.setHeaderText(null);
+        alert.setContentText(str);
+
+        // Optional: If you want the alert to be modal to the whole application, not just the current stage
+        alert.initModality(Modality.APPLICATION_MODAL);
+
+        alert.showAndWait();
+    }
+}
---
+++ src/cache/cacheparts/CacheEngine.java
@@ -0,0 +1,259 @@
+package cache.cacheparts;
+
+
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+
+import java.util.*;
+import java.util.List;
+
+public class CacheEngine
+{
+    private final CacheSimulator[] cacheSimulator;
+
+    public CacheEngine(int n){
+        this.cacheSimulator = new CacheSimulator[3];
+
+        //Test Case a: Sequential Sequence
+        Queue<Integer> sequentialSequence = generateSequentialSequence(n);
+        cacheSimulator[0] = new CacheSimulator(sequentialSequence);
+
+        //Test Case b: Random Sequence
+        Queue<Integer> randomSequence = generateRandomSequence(n);
+        cacheSimulator[1] = new CacheSimulator(randomSequence);
+
+        // Test Case c: Mid-repeat Blocks
+        Queue<Integer> midRepeatSequence = generateMidRepeatSequence(n);
+        cacheSimulator[2] = new CacheSimulator(midRepeatSequence);
+
+    }
+
+    public boolean performStep(List<List<List<Label>>> pane){
+        boolean flag = false;
+        int x = 0;
+        for(CacheSimulator simulator : cacheSimulator){
+            flag = flag | simulator.performStep();
+            //i = the two caches, 2.
+            //y = the block sizes
+            for(int i = 0; i < simulator.cacheSets.size(); i++){
+                for(int y = 0; y < simulator.cacheSets.get(i).size(); y++){
+                    String value = simulator.cacheSets.get(i).get(y).toString();
+                    int index = simulator.mostRecentlyAccessed[i];
+                    if(index == y){
+                        value = value + ", MRU";
+                    }
+                    pane.get(x).get(i).get(y).setText(value);
+                }
+            }
+            x++;
+        }
+        return flag;
+    }
+    public void performAllStep(TextArea area, List<List<List<Label>>> pane){
+        int id = 0;
+        StringBuilder builder = new StringBuilder();
+        for(CacheSimulator simulator : cacheSimulator){
+            //simulator.performAll();
+            switch (id) {
+                case 0 -> builder.append("Sequential Sequence");
+                case 1 -> builder.append("Random Sequence");
+                default -> builder.append("Mid-Repeat Sequence");
+            }
+            builder.append("\n");
+            while (!simulator.accessSequence.isEmpty()) {
+                performStep(pane);
+                getIndividualInformation(id,builder);
+            }
+            id++;
+        }
+        area.appendText(builder.toString());
+    }
+
+    private void getIndividualInformation(int id, StringBuilder builder){
+        CacheSimulator simulator = this.cacheSimulator[id];
+        builder.append("Cache State after Step ").append(simulator.currentStep).append(": ").append(simulator.cacheSets.toString());
+        builder.append("\n");
+        builder.append("Metrics after Step ").append(simulator.currentStep).append(":");
+        builder.append("\n");
+        builder.append("\tMRU Index Set 1: ").append(simulator.mostRecentlyAccessed[0]);
+        builder.append("\n");
+        builder.append("\tMRU Index Set 2: ").append(simulator.mostRecentlyAccessed[1]);
+        builder.append("\n");
+        builder.append("\tMemory Access Count: ").append(simulator.memoryAccessCount);
+        builder.append("\n");
+        builder.append("\tCache Hit Count and Hit Rate: ").append(simulator.cacheHitCount).append(" and ").append(((int)((simulator.cacheHitCount/(float)simulator.memoryAccessCount) * 10000))/100F).append("%");
+        builder.append("\n");
+        builder.append("\tCache Miss Count and Miss Rate: ").append(simulator.cacheMissCount).append(" and ").append(((int)((simulator.cacheMissCount/(float)simulator.memoryAccessCount) * 10000))/100F).append("%");
+        builder.append("\n");
+        builder.append("\tTotal Memory Access Time: ").append(simulator.totalMemoryAccessTime);
+        builder.append("\n");
+
+        //72
+        int average = (int)((simulator.cacheHitCount / (float)simulator.memoryAccessCount) + (simulator.cacheMissCount/ (float)simulator.memoryAccessCount) * (1 + (simulator.cacheMissCount * 10)));
+
+        builder.append("\tAverage Memory Access Time: ").append(average);
+        builder.append("\n");
+
+
+    }
+    public String getCurrentStepInformation(){
+        StringBuilder builder = new StringBuilder();
+        int id = 0;
+        for(CacheSimulator simulator : cacheSimulator){
+            switch (id) {
+                case 0 -> builder.append("Sequential Sequence");
+                case 1 -> builder.append("Random Sequence");
+                default -> builder.append("Mid-Repeat Sequence");
+            }
+            builder.append("\n");
+            id++;
+            builder.append("Cache State after Step ").append(simulator.currentStep).append(": ").append(simulator.cacheSets.toString());
+            builder.append("\n");
+            builder.append("Metrics after Step ").append(simulator.currentStep).append(":");
+            builder.append("\n");
+            builder.append("\tMRU Index Set 1: ").append(simulator.mostRecentlyAccessed[0]);
+            builder.append("\n");
+            builder.append("\tMRU Index Set 2: ").append(simulator.mostRecentlyAccessed[1]);
+            builder.append("\n");
+            builder.append("\tMemory Access Count: ").append(simulator.memoryAccessCount);
+            builder.append("\n");
+            builder.append("\tCache Hit Count and Hit Rate: ").append(simulator.cacheHitCount).append(" and ").append(((int)((simulator.cacheHitCount/(float)simulator.memoryAccessCount) * 10000))/100F).append("%");
+            builder.append("\n");
+            builder.append("\tCache Miss Count and Miss Rate: ").append(simulator.cacheMissCount).append(" and ").append(((int)((simulator.cacheMissCount/(float)simulator.memoryAccessCount) * 10000))/100F).append("%");
+            builder.append("\n");
+            builder.append("\tTotal Memory Access Time: ").append(simulator.totalMemoryAccessTime);
+            builder.append("\n");
+
+            int average = (int)((simulator.cacheHitCount / (float)simulator.memoryAccessCount) + (simulator.cacheMissCount/ (float)simulator.memoryAccessCount) * (1 + (simulator.cacheMissCount * 10)));
+            builder.append("\tAverage Memory Access Time: ").append(average);
+            builder.append("\n");
+        }
+
+        return builder.toString();
+    }
+
+    public static Queue<Integer> generateSequentialSequence(int n) {
+        Queue<Integer> sequence = new LinkedList<>();
+        for (int i = 0; i < 4; i++) { // Repeat 4 times
+            for (int j = 0; j < n*2; j++) {
+                sequence.add(j);
+            }
+        }
+        System.out.println("Sequence: " + sequence);
+        return sequence;
+    }
+
+    public static Queue<Integer> generateRandomSequence(int size) {
+        Queue<Integer> sequence = new LinkedList<>();
+        Random random = new Random();
+        for (int i = 0; i < size*4; i++) {
+            sequence.add(random.nextInt(32));
+        }
+        System.out.println("Sequence: " + sequence);
+        return sequence;
+    }
+    public static Queue<Integer> generateCustom(int n) {
+        Queue<Integer> sequence = new LinkedList<>();
+        for(int i = 0; i < 16; i++){
+            sequence.add(i);
+        }
+        sequence.add(0);
+        sequence.add(1);
+        for(int i = 16; i < 32; i++){
+            sequence.add(i);
+        }
+
+        System.out.println("Sequence: " + sequence);
+        return sequence;
+    }
+
+    private static Queue<Integer> generateMidRepeatSequence(int n) {
+        Queue<Integer> sequence = new LinkedList<>();
+        for (int i = 0; i < 4; i++) { // Repeat 4 times
+            // Start with 0
+            sequence.add(0);
+            // Repeat the middle sequence (numbers 1 to n-1) twice
+            for(int z = 0; z < 2; z ++){
+                int loop = z == 1 ? n : n - 1;
+                for (int j = 1; j < loop; j++) {
+                    sequence.add(j);
+                }
+            }
+            // Continue the sequence up to 2n-1
+            for (int j = n; j < 2 * n; j++) {
+                sequence.add(j);
+            }
+        }
+        System.out.println("Sequence: " + sequence);
+        return sequence;
+    }
+    public static class CacheSimulator {
+        private static final int CACHE_LINES_PER_BLOCK = 32; // 32 words per cache line
+        private static final int NUMBER_OF_CACHE_BLOCKS = 16; // Total number of cache blocks
+        private final List<List<Integer>> cacheSets; // 8-way set associative cache
+        private static final int EIGHT_WAY = 8;
+
+        private final Queue<Integer> accessSequence;
+        private int memoryAccessCount = 0;
+        private int cacheHitCount = 0;
+        private int cacheMissCount = 0;
+        private int totalMemoryAccessTime = 0;
+        private int currentStep = 0;
+
+        private final int [] mostRecentlyAccessed;
+
+        public CacheSimulator(Queue<Integer> accessSequence) {
+            int numberOfSets = NUMBER_OF_CACHE_BLOCKS / EIGHT_WAY; // Assuming 8-way set associative cache
+
+            this.accessSequence = new LinkedList<>(accessSequence);
+            this.cacheSets = new ArrayList<>(numberOfSets);
+            for (int i = 0; i < numberOfSets; i++) {
+                cacheSets.add(new ArrayList<>()); // Initialize each set
+            }
+            mostRecentlyAccessed = new int[this.cacheSets.size()];
+        }
+
+        public boolean performStep() {
+            if (accessSequence.isEmpty()) {
+                return false;
+            }
+
+            int blockToAccess = accessSequence.poll();
+            processBlockAccess(blockToAccess);
+
+            currentStep++;
+            return true;
+        }
+        private void processBlockAccess(int blockToAccess) {
+            int setIndex = blockToAccess % cacheSets.size(); //4%2=0
+            List<Integer> set = cacheSets.get(setIndex); //0
+
+            memoryAccessCount++;
+
+            boolean isHit = set.contains(blockToAccess);//does have 4?
+            if (isHit) {
+                mostRecentlyAccessed[setIndex] = set.indexOf(blockToAccess);
+                cacheHitCount++;
+                totalMemoryAccessTime = (cacheHitCount * 2) + (cacheMissCount * 2 * 11) + (cacheMissCount); // Assumed time for memory access
+            } else {
+                cacheMissCount++;
+                totalMemoryAccessTime = (cacheHitCount * 2) + (cacheMissCount * 2 * 11) + (cacheMissCount); // Assumed time for memory access
+                //Fill in while not yet full...
+                if(set.size() < EIGHT_WAY){
+                    mostRecentlyAccessed[setIndex] = set.size();
+                    set.add(blockToAccess);
+                }
+                else {
+                    //Remove the most recently used index, and replace it with the memory queried
+                    set.remove(mostRecentlyAccessed[setIndex]);
+                    set.add(mostRecentlyAccessed[setIndex], blockToAccess);
+                }
+
+            }
+        }
+
+    }
+}
+
+
+
